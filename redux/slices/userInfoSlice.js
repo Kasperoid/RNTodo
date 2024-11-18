14a14c5d4934f1f85b88163879259a11b21da2db
Kasperoid <ivanu9053@gmail.com>
2024-11-18 11:45:57 +0300

    Создание слайса для пользователей

---
+++ redux/slices/userInfoSlice.js
@@ -0,0 +1,9 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const userInfoSlice = createSlice({
+  name: 'userInfo',
+  initialState: {},
+  reducers: {},
+});
+
+export default userInfoSlice.reducer;
