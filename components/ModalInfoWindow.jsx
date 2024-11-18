7ccb27d130a27454225afa6d153d8a11861ed046
Kasperoid <ivanu9053@gmail.com>
2024-11-18 11:47:12 +0300

    Создание компоненты модального окна с отображением информации

---
+++ components/ModalInfoWindow.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Modal, View, Text} from 'react-native';
+import {styles} from '../styles/styles';
+import {ButtonUI} from './UI/ButtonUI';
+
+export const ModalInfoWindow = ({isOpened, setCloseFunc, message, title}) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isOpened}
+      onRequestClose={setCloseFunc}>
+      <View style={styles.modalBackground}>
+        <View style={styles.modalInnerContainer}>
+          <Text
+            style={[
+              {
+                textAlign: 'center',
+              },
+              styles.titleH1,
+            ]}>
+            {title}
+          </Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 16,
+            }}>
+            {message}
+          </Text>
+          <ButtonUI onPressFunc={setCloseFunc} type={'Primary'}>
+            Закрыть
+          </ButtonUI>
+        </View>
+      </View>
+    </Modal>
+  );
+};
