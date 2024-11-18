3b1c20ca38e2d4ec9a7cb876da937815549e0d45
Kasperoid <ivanu9053@gmail.com>
2024-11-18 11:46:53 +0300

    Создание компоненты стилизованной кнопки

---
+++ components/UI/ButtonUI.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {TouchableHighlight, View, Text} from 'react-native';
+import {styles} from '../../styles/styles';
+
+export const ButtonUI = ({children, onPressFunc, type}) => {
+  return (
+    <TouchableHighlight
+      onPress={onPressFunc}
+      activeOpacity={0.9}
+      underlayColor={'#874f1e'}
+      style={styles.buttonTouchableContainer}>
+      <View style={[styles[`button${type}`], styles.buttonInnerContainer]}>
+        <Text
+          style={[
+            {
+              color: type === 'Primary' ? 'white' : '#e28533',
+            },
+            styles.textButton,
+          ]}>
+          {children}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
