7df1c50fa2f55ce4bc7ff28040210f2cc4c170c8
Kasperoid <ivanu9053@gmail.com>
2024-11-18 11:48:24 +0300

    Добавление валидации почты, редакса, модальных окон

--- components/LogInPage.jsx
+++ components/LogInPage.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {Pressable, TextInput} from 'react-native-gesture-handler';
+import {View, Text, TextInput} from 'react-native';
 import {styles} from '../styles/styles';
+import {ModalInfoWindow} from './ModalInfoWindow';
+import {ButtonUI} from './UI/ButtonUI';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  setErrorMessage,
+  setIsError,
+  setLoginInput,
+  setPswdInput,
+} from '../redux/slices/logInPageSlice';
 
-export function LogInPage() {
+export function LogInPage({navigation}) {
+  const dispatch = useDispatch();
+  const {logInInput, pswdInput, isError, errorMessage} = useSelector(
+    store => store.logInPage,
+  );
+
+  function isRuEmail(str) {
+    var emailText = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.ru$/);
+    return emailText.test(str);
+  }
+
+  function logBtnHandler() {
+    if (isRuEmail(logInInput)) {
+      dispatch(setIsError(false));
+      navigation.navigate('Home');
+    } else {
+      dispatch(setIsError(true));
+      dispatch(setErrorMessage('Введите корректный e-mail'));
+    }
+  }
+
+  function createBtnHandler() {
+    if (isRuEmail(logInInput)) {
+      dispatch(setIsError(false));
+      navigation.navigate('Home');
+    } else {
+      dispatch(setIsError(true));
+      dispatch(setErrorMessage('Введите корректный e-mail'));
+    }
+  }
+
   return (
     <View style={[styles.pageContainer, styles.pageContainerItemCenter]}>
+      <ModalInfoWindow
+        isOpened={isError}
+        setCloseFunc={() => dispatch(setIsError(false))}
+        message={errorMessage}
+        title={'Ошибка'}
+      />
       <View style={styles.logInContainer}>
         <Text
           style={[
@@ -17,44 +61,31 @@ export function LogInPage() {
           Вход
         </Text>
         <View style={{gap: 10}}>
-          <TextInput placeholder="Логин" style={styles.inputTextCustom} />
-          <TextInput placeholder="Пароль" style={styles.inputTextCustom} />
+          <TextInput
+            textContentType="emailAddress"
+            onChangeText={text => dispatch(setLoginInput(text))}
+            placeholder="Логин"
+            style={styles.inputTextCustom}
+            value={logInInput}
+          />
+          <TextInput
+            textContentType="password"
+            onChangeText={text => dispatch(setPswdInput(text))}
+            value={pswdInput}
+            placeholder="Пароль"
+            style={styles.inputTextCustom}
+          />
         </View>
         <View
           style={{
-            flexDirection: 'row',
-            gap: 35,
+            gap: 10,
           }}>
-          <Pressable
-            onPress={() => console.log()}
-            style={({pressed}) => [
-              {
-                borderColor: 'white',
-                backgroundColor: pressed ? '#9c5a20' : '#e28533',
-              },
-              styles.button,
-            ]}>
-            <Text
-              style={[
-                {
-                  color: 'white',
-                },
-                styles.textButton,
-              ]}>
-              Войти
-            </Text>
-          </Pressable>
-          <Pressable
-            onPress={() => console.log()}
-            style={({pressed}) => [
-              {
-                backgroundColor: pressed ? '#faf7f3' : 'white',
-                borderColor: '#e28533',
-              },
-              styles.button,
-            ]}>
-            <Text style={styles.textButton}>Создать</Text>
-          </Pressable>
+          <ButtonUI onPressFunc={() => logBtnHandler()} type={'Primary'}>
+            Войти
+          </ButtonUI>
+          <ButtonUI onPressFunc={createBtnHandler} type={'Secondary'}>
+            Создать
+          </ButtonUI>
         </View>
       </View>
     </View>
