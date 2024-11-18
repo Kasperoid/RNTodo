9dad3dd872518e9b4b090da36f1e5fdb5d178079
Kasperoid <ivanu9053@gmail.com>
2024-11-18 11:49:17 +0300

    Создание нового маршрута и подключение к проекту редакса

--- App.jsx
+++ App.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {LogInPage} from './components/LogInPage';
+import {HomePage} from './components/HomePage';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 
 export default function App() {
   const {Navigator, Screen} = createStackNavigator();
   return (
-    <NavigationContainer>
-      <Navigator initialRouteName="LogIn">
-        <Screen name="LogIn" component={LogInPage} options={{title: 'Вход'}} />
-      </Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Navigator initialRouteName="LogIn">
+          <Screen
+            name="LogIn"
+            component={LogInPage}
+            options={{headerShown: false}}
+          />
+          <Screen
+            name="Home"
+            options={{headerShown: false}}
+            component={HomePage}
+          />
+        </Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
