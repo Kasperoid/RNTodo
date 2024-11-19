a27ef8ebd73d729f8f5d0c4ef60f0323942a406b
Kasperoid <ivanu9053@gmail.com>
2024-11-19 11:30:21 +0300

    Конфигурирование redux-persist

--- App.jsx
+++ App.jsx
@@ -2,28 +2,38 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {LogInPage} from './components/LogInPage';
-import {HomePage} from './components/HomePage';
 import {Provider} from 'react-redux';
-import {store} from './redux/store';
+import store, {persistor} from './redux/store';
+import {TodosList} from './components/TodosList';
+import {HomePage} from './components/HomePage';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Text} from 'react-native';
 
 export default function App() {
   const {Navigator, Screen} = createStackNavigator();
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Navigator initialRouteName="LogIn">
-          <Screen
-            name="LogIn"
-            component={LogInPage}
-            options={{headerShown: false}}
-          />
-          <Screen
-            name="Home"
-            options={{headerShown: false}}
-            component={HomePage}
-          />
-        </Navigator>
-      </NavigationContainer>
+      <PersistGate loading={<Text>Загрузка...</Text>} persistor={persistor}>
+        <NavigationContainer>
+          <Navigator initialRouteName="LogIn">
+            <Screen
+              name="LogIn"
+              component={LogInPage}
+              options={{headerShown: false}}
+            />
+            <Screen
+              name="Home"
+              options={{headerShown: false}}
+              component={HomePage}
+            />
+            <Screen
+              name="TodosList"
+              options={{headerShown: false}}
+              component={TodosList}
+            />
+          </Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,40 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import todosListSlice from './slices/todosListSlice';
 import userInfoSlice from './slices/userInfoSlice';
 
-export const store = configureStore({
-  reducer: {
-    todosList: todosListSlice,
-    userInfo: userInfoSlice,
-  },
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  todosList: todosListSlice,
+  userInfo: userInfoSlice,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
