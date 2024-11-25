b0eaf44228e036ad2842f0c96ee3a1a741e1ab6a
Kasperoid <ivanu9053@gmail.com>
2024-11-25 11:17:56 +0300

    Добавление на страницу с описанием задачи функцию удаления

--- components/TodoDescPage.jsx
+++ components/TodoDescPage.jsx
@@ -5,9 +5,10 @@ import {useDispatch, useSelector} from 'react-redux';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {TextInput} from 'react-native-gesture-handler';
 import {ButtonUI} from './UI/ButtonUI';
-import {setDescTodo} from '../redux/slices/todosListSlice';
+import {deleteTodo, setDescTodo} from '../redux/slices/todosListSlice';
+import {removeCountTodo} from '../redux/slices/tagsListSlice';
 
-export const TodoDescPage = () => {
+export const TodoDescPage = ({navigation}) => {
   const confirmBtnHandler = () => {
     dispatch(setDescTodo(inputDescValue));
     setIsOpenInput(false);
@@ -16,6 +17,12 @@ export const TodoDescPage = () => {
   const cancelBtnHandler = () => {
     setIsOpenInput(prevState => !prevState);
     setInputDescValue(selectedTodo?.desc || '');
+  };
+
+  const deleteTagBtnHandler = () => {
+    dispatch(deleteTodo(selectedTodo.id));
+    dispatch(removeCountTodo());
+    navigation.navigate('TodosList');
   };
 
   const dispatch = useDispatch();
@@ -29,57 +36,66 @@ export const TodoDescPage = () => {
   const [isOpenInput, setIsOpenInput] = useState(false);
   return (
     <View style={styles.pageContainer}>
-      <Text style={[styles.titleH1, {textAlign: 'center', marginBottom: 0}]}>
-        {selectedTodo.title}
-      </Text>
-      <View style={styles.separator} />
-      {isOpenInput ? (
-        <View>
-          <TextInput
-            editable
-            multiline
-            numberOfLines={4}
-            onChangeText={text => setInputDescValue(text)}
-            value={inputDescValue}
-            style={styles.inputTextCustom}
-            placeholder="Введи описание!"
-          />
-          <View style={{gap: 5, marginTop: 15}}>
-            <ButtonUI type={'Primary'} onPressFunc={() => confirmBtnHandler()}>
-              Ввести
-            </ButtonUI>
-            <ButtonUI type={'Secondary'} onPressFunc={() => cancelBtnHandler()}>
-              Отменить
-            </ButtonUI>
-          </View>
-        </View>
-      ) : selectedTodo?.desc ? (
-        <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
-          <Text style={styles.commonText}>{selectedTodo.desc}</Text>
-          <TouchableHighlight
-            underlayColor={null}
-            onPress={() => setIsOpenInput(prevState => !prevState)}
-            onPressIn={() => setActiveBtn(true)}
-            onPressOut={() => setActiveBtn(false)}>
-            <AntDesign
-              name="edit"
-              size={18}
-              color={activeBtn ? '#e28533' : 'black'}
+      <View style={{flex: 1}}>
+        <Text style={[styles.titleH1, {textAlign: 'center', marginBottom: 0}]}>
+          {selectedTodo.title}
+        </Text>
+        <View style={styles.separator} />
+        {isOpenInput ? (
+          <View>
+            <TextInput
+              editable
+              multiline
+              numberOfLines={4}
+              onChangeText={text => setInputDescValue(text)}
+              value={inputDescValue}
+              style={styles.inputTextCustom}
+              placeholder="Введи описание!"
             />
-          </TouchableHighlight>
-        </View>
-      ) : (
-        <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
-          <Text style={styles.commonText}>{'Описания нет...'}</Text>
-          <TouchableHighlight
-            onPress={() => setIsOpenInput(prevState => !prevState)}
-            underlayColor={null}
-            onPressIn={() => setActiveBtn(true)}
-            onPressOut={() => setActiveBtn(false)}>
-            <Text style={{color: '#e28533', fontWeight: 700}}>Добавить?</Text>
-          </TouchableHighlight>
-        </View>
-      )}
+            <View style={{gap: 5, marginTop: 15}}>
+              <ButtonUI
+                type={'Primary'}
+                onPressFunc={() => confirmBtnHandler()}>
+                Ввести
+              </ButtonUI>
+              <ButtonUI
+                type={'Secondary'}
+                onPressFunc={() => cancelBtnHandler()}>
+                Отменить
+              </ButtonUI>
+            </View>
+          </View>
+        ) : selectedTodo?.desc ? (
+          <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
+            <Text style={styles.commonText}>{selectedTodo.desc}</Text>
+            <TouchableHighlight
+              underlayColor={null}
+              onPress={() => setIsOpenInput(prevState => !prevState)}
+              onPressIn={() => setActiveBtn(true)}
+              onPressOut={() => setActiveBtn(false)}>
+              <AntDesign
+                name="edit"
+                size={18}
+                color={activeBtn ? '#e28533' : 'black'}
+              />
+            </TouchableHighlight>
+          </View>
+        ) : (
+          <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
+            <Text style={styles.commonText}>{'Описания нет...'}</Text>
+            <TouchableHighlight
+              onPress={() => setIsOpenInput(prevState => !prevState)}
+              underlayColor={null}
+              onPressIn={() => setActiveBtn(true)}
+              onPressOut={() => setActiveBtn(false)}>
+              <Text style={{color: '#e28533', fontWeight: 700}}>Добавить?</Text>
+            </TouchableHighlight>
+          </View>
+        )}
+      </View>
+      <ButtonUI type={'Primary'} onPressFunc={() => deleteTagBtnHandler()}>
+        Удалить задачу
+      </ButtonUI>
     </View>
   );
 };
